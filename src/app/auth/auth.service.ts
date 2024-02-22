9c1bd4e2cf8bd123a9c9b6270033e1def6a6449c
unknown <c.cruzmarcial2001@gmail.com>
2024-02-22 12:51:20 -0600

    added auto logout and auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -20,84 +20,117 @@ export interface AuthResponseData {
 
 export class AuthService {
   user = new BehaviorSubject<User>(null)
+  tokenExpiration: any
 
 
-  constructor(private http: HttpClient, private router: Router) {}
+constructor(private http: HttpClient, private router: Router) {}
 
-    signup(email: string, password: string) {
-       return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSkOh_-im3wgR6qhQ14qL4BkDDacD-OnI',
-        {
-          email: email,
-          password: password,
-          returnSecureToken: true
-        }
-        ).pipe(catchError(this.handleError),
-        tap(resData => {
-          this.handleAuthentication(
-            resData.email,
-            resData.localId,
-            resData.idToken,
-            +resData.expiresIn
-            );
-        })
-        );
-    }
-    login(email: string, password: string) {
-      return this.http
-      .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBSkOh_-im3wgR6qhQ14qL4BkDDacD-OnI',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true
-      }
-    ).pipe(catchError(this.handleError),
+signup(email: string, password: string) {
+  return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBSkOh_-im3wgR6qhQ14qL4BkDDacD-OnI',
+  {
+    email: email,
+    password: password,
+    returnSecureToken: true
+  }
+  ).pipe(catchError(this.handleError),
     tap(resData => {
-      this.handleAuthentication(
-        resData.email,
-        resData.localId,
-        resData.idToken,
-        +resData.expiresIn
-        );
+    this.handleAuthentication(
+      resData.email,
+      resData.localId,
+      resData.idToken,
+      +resData.expiresIn
+      );
     })
-    )
-  }
-
-  logout(){
-    this.user.next(null);
-    this.router.navigate(['/auth']);
-  }
-    private handleAuthentication(
-      email: string,
-      userId: string,
-      token: string,
-      expiresIn: number){
-      const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
-      const user = new User
-      (
-        email,
-        userId,
-        token,
-        expirationDate
-        );
-        this.user.next(user);
+  );
+}
+login(email: string, password: string) {
+  return this.http
+    .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBSkOh_-im3wgR6qhQ14qL4BkDDacD-OnI',
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true
     }
+  ).pipe(catchError(this.handleError),
+  tap(resData => {
+    this.handleAuthentication(
+      resData.email,
+      resData.localId,
+      resData.idToken,
+      +resData.expiresIn
+      );
+    })
+  )
+}
 
-      private handleError(errorRes: HttpErrorResponse) {
-        let errorMessage: string = "An unknown error occurred!";
-        if(!errorRes.error || !errorRes.error.error){
-          return throwError(() => errorMessage);
-        }
-        switch(errorRes.error.error.message){
-          case 'EMAIL_EXISTS':
-            errorMessage = "This email already exists!";
-            break;
-          case 'EMAIL_NOT_FOUND':
-            errorMessage = "This email does not exist!";
-            break;
-          case 'INVALID_PASSWORD':
-            errorMessage = "This password is not correct!";
-            break;
-        }
-        return throwError(() => errorMessage);
-      }
+autoLogin(){
+  const userData: {
+    email: string;
+    id: string;
+    _token: string;
+    _tokenExpirationDate: string;
+  } = JSON.parse(localStorage.getItem('userData'));
+  if(!userData){
+    return;
+  }
+  const loadedUser = new User(
+    userData.email,
+    userData.id,
+    userData._token,
+    new Date(userData._tokenExpirationDate)
+    );
+    if(loadedUser.token){
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration)
+    }
+}
+
+logout(){
+  this.user.next(null);
+  this.router.navigate(['/auth']);
+  localStorage.removeItem('userData');
+  if(this.tokenExpiration){
+    clearTimeout(this.tokenExpiration);
+  }
+  this.tokenExpiration = null;
+}
+
+autoLogout(expirationDuration: number){
+  this.tokenExpiration = setTimeout(() => {
+    this.logout();
+  }, expirationDuration);
+
+}
+
+private handleAuthentication(
+  email: string,
+  userId: string,
+  token: string,
+  expiresIn: number
+  ) {
+  const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+  const user = new User(email, userId, token, expirationDate);
+  this.user.next(user);
+  this.autoLogout(expiresIn * 1000);
+  localStorage.setItem('userData', JSON.stringify(user));
+  }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage: string = "An unknown error occurred!";
+    if(!errorRes.error || !errorRes.error.error){
+      return throwError(() => errorMessage);
+    }
+    switch(errorRes.error.error.message){
+      case 'EMAIL_EXISTS':
+        errorMessage = "This email already exists!";
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "This email does not exist!";
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = "This password is not correct!";
+        break;
+    }
+    return throwError(() => errorMessage);
+  }
 }
